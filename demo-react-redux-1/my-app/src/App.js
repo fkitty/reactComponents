a7ea39ffbe3185d3c0cddbcc025075cee00dd239
脱安琪 <453001363@qq.com>
2020-09-07 23:45:33 +0800

    feat: redux+react实现count

--- demo-react-redux-1/my-app/src/App.js
+++ demo-react-redux-1/my-app/src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      你点击了<span id="value">0</span>次
+class App extends Component{
+
+  add1(){
+    // store.dispatch({
+    // })
+    console.log(this.props.onAdd1, '9999');
+    this.props.onAdd1();
+  }
+  add2(){
+    this.props.onAdd2();
+  }
+  add3(){
+    this.props.onAdd3();
+  }
+  add4(){
+    this.props.onAdd4();
+  }
+  render(){
+    return (
+      <div className="App">
+        你点击了<span id="value">{this.props.value}</span>次
           <div>
-            <button id="add1">+1</button>
-            <button id="add2">+2</button>
-            <button id="add1IfOdd">如果是单数就+1</button>
-            <button id="add1After2Sec">两秒钟后+1</button>
+            <button id="add1" onClick={() => this.add1()}>+1</button>
+            <button id="add2" onClick={() => this.add2()}>+2</button>
+            <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
+            <button id="add1After2Sec" onClick={() => this.add4()}>两秒钟后+1</button>
           </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
