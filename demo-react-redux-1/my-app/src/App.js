200b66d378952393dc91cf325da5826c21f45449
脱安琪 <453001363@qq.com>
2020-09-13 18:05:09 +0800

    fix: 修改connect参数名，把第二个参数改写成函数

--- demo-react-redux-1/my-app/src/App.js
+++ demo-react-redux-1/my-app/src/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
+import {
+  connect,
+} from 'react-redux';
 
 class App extends Component{
-
-  add1(){
-    // store.dispatch({
-    // })
-    this.props.onAdd1();
-    // const { store } = this.props;
-    // store.dispatch({
-    //   type: 'add',
-    //   payload: 1,
-    // })
-  }
-  add2(){
-    this.props.onAdd2();
-  }
-  add3(){
-    this.props.onAdd3();
-  }
-  add4(){
-    this.props.onAdd4();
-  }
   render(){
     return (
       <div className="App">
-        你点击了<span id="value">{this.props.value}</span>次
+        你点击了<span id="value">{this.props.n}</span>次
           <div>
-            <button id="add1" onClick={() => this.add1()}>+1</button>
-            <button id="add2" onClick={() => this.add2()}>+2</button>
-            <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
-            <button id="add1After2Sec" onClick={() => this.add4()}>两秒钟后+1</button>
+            <button id="add1" onClick={this.props.add1}>+1</button>
+            <button id="add2" >+2</button>
+            <button id="add1IfOdd" >如果是单数就+1</button>
+            <button id="add1After2Sec" >两秒钟后+1</button>
 
             {/* <Child1 store={this.props.store}/> */}
           </div>
@@ -43,11 +25,43 @@ class App extends Component{
 // x是一个函数，返回一个对象
 // 从state中拿出一部分状态
 // 旧的state, 返回：从state中拿出什么值
-function x(state){
+// x是把state的东西拿出来，放到this的props上
+function mapStateToProps(state){ // 获取部分store
   return {
     n: state.n,
   }
 }
 
-connect(x)(App); // 第一个参数，第二个参数
-export default App;
+// 生产一个action  不是函数，是一个对象
+// function y(){
+//   return{
+//     add1: () => {
+//       return{
+//         type: 'add',
+//         payload: 1,
+//       }
+//     }
+//   }
+// }
+
+// const actionCreator = { // 需要生成哪些action
+//   add1: () => {
+//     return{
+//       type: 'add',
+//       payload: 1,
+//     }
+//   }
+// }
+
+// 把对象改成函数
+function mapDispatchToprops(dispatch){
+  return{
+    add1: () => dispatch({
+      type: 'add',
+      payload: 1,
+    })
+  }
+}
+
+// connect(x)(App); // 第一个参数，第二个参数
+export default connect(mapStateToProps, mapDispatchToprops)(App);
--- demo-react-redux-1/my-app/src/index.js
+++ demo-react-redux-1/my-app/src/index.js
@@ -8,18 +8,17 @@ import { Provider } from 'react-redux';
 
 const stateChanger = (state, action) => {
   if (typeof state === 'undefined') {
-    return 0
+    return {n: 0}
   } else {
     if (action.type === 'add') {
-      var newState = state + action.payload;
+      // var newState = state + action.payload;
+      var newState = {n: state.n + action.payload}; // 新的state和旧的state是一样的结构
       return newState; // 2、根据操作生成新的state 触发一个事件
     }
     else {
       return state;
     }
   }
-
-
 }
 
 const store = createStore(stateChanger);
@@ -29,29 +28,6 @@ const store = createStore(stateChanger);
 // store.subscribe(() => {
 //   render();
 // })
-
-// 这样写是错的
-// store.subscribe(render()) // 先执行render,把render的结果传给subscribe
-
-// store.subscribe(render) //对的
-
-function add3() {
-  if( store.getState() % 2 === 1){
-    store.dispatch({
-      type: 'add',
-      payload: 1,
-    })
-  }
-}
-
-function add4() {
-  window.setTimeout(() => {
-    store.dispatch({
-      type: 'add',
-      payload: 1,
-    })
-  }, 2000)
-}
 
 // function render() {
   ReactDOM.render(
